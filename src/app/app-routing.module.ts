128845664ddc0d7de0530bbded2c3116359070b5
sarahborkan <sarah.behaz1@gmail.com>
2023-07-03 10:55:59 +0200

    ajout des composants et routing de la page infos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfosComponent } from './infos/infos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "infos", component : InfosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { InfosComponent } from './infos/infos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    InfosComponent
   ],
   imports: [
     BrowserModule,
